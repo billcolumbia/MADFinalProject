7b5f2689d7720d8f3c919b4d436add3f960967a8
Juan Valera <jav094@gmail.com>
2012-10-23 14:15:27 -0400

    Initial Commit

---
+++ Final Project/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Final Project
+//
+//  Created by Juan Valera on 10/23/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
